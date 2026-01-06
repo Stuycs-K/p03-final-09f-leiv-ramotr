e708e59a2bac75e1dce34b1191f333473535cbe8
Vanna Lei <vanna.lei123@gmail.com>
2026-01-06 14:50:31 -0500

    started game

---
+++ game.c
@@ -0,0 +1,20 @@
+#include "networking.c"
+
+int start_game(){
+
+}
+
+void game_statements(){
+  char line[10];
+  fgets(line, )
+  printf("Would you like to play? (y/n)");
+  if(line[0] == "y"){
+    start_game();
+  }
+  if(line[0] == "n"){
+    exit(0);
+  }
+  else{
+
+  }
+}
