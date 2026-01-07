4eb5dece3f4a2a176c7ad39abcef0fbfb80093ce
Raphael Ramot <raphaelr20@nycstudents.net>
2026-01-07 13:42:47 -0600

    working on select

--- server.c
+++ server.c
@@ -14,7 +14,18 @@ int main(int argc, char *argv[] ) {
   signal(SIGINT,sighandler);
   int listen_socket = server_setup();
   printf("listening for connections...\n");
+  char buf[1025];
   while(1) {
+    fd_set descriptors;
+    FD_ZERO(&descriptors);
+    FD_SET(listen_socket,&descriptors);
+    FD_SET(STDIN_FILENO,&descriptors);
+    int i = select(listen_socket+1,&descriptors,NULL,NULL,NULL);
+    if (FD_ISSET(STDIN_FILENO,&descriptors)) {
+      fgets(buf,sizeof(buf),stdin);
+      buf[strlen(buf)-1]=0;
+      printf("%s\n",buf); // put the game function here
+    }
     int client_socket = server_tcp_handshake(listen_socket);
     printf("client connected\n");
     pid_t p = fork();
