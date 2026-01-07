9843d67d33fb0f0094fba12f7fa170f9f053d485
Vanna Lei <vanna.lei123@gmaill.com>
2026-01-07 00:48:22 -0500

    worked on printing game statements at the very beginning after client has been found

--- game.c
+++ game.c
@@ -1,20 +1,30 @@
-#include "networking.c"
-
-int start_game(){
-
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+//#include "networking.h"
 
 void game_statements(){
   char line[10];
-  fgets(line, )
-  printf("Would you like to play? (y/n)");
-  if(line[0] == "y"){
-    start_game();
+  while(1){
+    printf("Would you like to play? (y/n)\n");
+    if(fgets(line, 10, stdin) == NULL){
+      continue;
+    }
+    if(line[0] == 'y'){
+      //start_game();
+      printf("start game\n");
+    }
+    else if(line[0] == 'n'){
+      exit(0);
+    }
+    else{
+      printf("please type only y (for yes) or n (for no) \n Would you like to play? (y/n)");
+    }
   }
-  if(line[0] == "n"){
-    exit(0);
-  }
-  else{
+}
 
-  }
+int main(){
+  game_statements();
+  return 0;
 }
