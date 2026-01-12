473721d6bfe8e8b3bfa88c3d573a3700bb274221
Vanna Lei <vanna.lei123@gmail.com>
2026-01-12 00:38:04 -0500

    added help manual

--- game.c
+++ game.c
@@ -15,7 +15,15 @@ void print_board(){
 }
 
 void print_help() {
-  
+  printf("1. Playing locally means you are playing on the same computer with your opponent.\n Playing against a random opponent means you will be connected to a random server or one of your choice\n");
+  printf("2. To play the game, type the coordinates of an empty box.\n");
+  printf("Top (T), Middle(M), Bottom (B), Left (L), Right(R)\n");
+  printf("TL | TM | TR \n———|————|——— \nML | MM | MR \n———|————|——— \nBL | BM | BR\n");
+  printf("3. If already taken, you will be prompted to choose a different spot.\n");
+  printf("4. Wait for your opponent's turn and repeat.\n");
+  printf("5. Have your pieces three in a row or diagonally to win.\n");
+  printf("6. When the game is over, you will have the option to play again with the same client, never play again with the same client, or connect with a random person.\n");
+
 }
 
 void reset_board() {
