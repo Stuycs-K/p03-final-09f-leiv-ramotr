c2e740e7a474752ae284a79a753c782612ef2cca
Raphael Ramot <raphaelr20@nycstudents.net>
2026-01-12 02:45:57 -0500

    adding another fdset to reset after checking

--- server.c
+++ server.c
@@ -17,8 +17,8 @@ void matchmaking(int fd){
   opponent[fd] = waiting_fd;
   opponent[waiting_fd] = fd;
   int p1 = 1, p2 = 2;
-  send(fd,&p1,sizeof(int),0);
-  send(waiting_fd,&p2,sizeof(int),0);
+  send(fd,&p2,sizeof(int),0);
+  send(waiting_fd,&p1,sizeof(int),0);
   waiting_fd = -1;
 }
 
@@ -26,6 +26,8 @@ void close_client(int fd, fd_set *descriptors) {
   FD_CLR(fd,descriptors);
   close(fd);
   opponent[fd] = -1;
+  // reset waiting_fd if it was waiting
+  if (waiting_fd==fd)waiting_fd = -1;
   printf("client disconnected\n");
 }
 
@@ -34,7 +36,7 @@ int main(int argc, char *argv[] ) {
   int listen_socket = server_setup();
   printf("listening for connections...\n");
   char buf[1025];
-  fd_set descriptors;
+  fd_set descriptors, current;
   FD_ZERO(&descriptors);
   FD_SET(listen_socket,&descriptors);
   int max_fd = listen_socket;
@@ -42,14 +44,16 @@ int main(int argc, char *argv[] ) {
     opponent[i] = -1;
   }
   while(1) {
-    int sel = select(max_fd+1,&descriptors,NULL,NULL,NULL);
+    current = descriptors;
+    int sel = select(max_fd+1,&current,NULL,NULL,NULL);
     if (sel<0)err();
     for (int fd = 0; fd<max_fd+1; fd++) {
-      if (!FD_ISSET(fd,&descriptors)) continue;
+      if (!FD_ISSET(fd,&current)) continue;
       if (fd==listen_socket) {
         // add new clients to list
         int client_socket = server_tcp_handshake(listen_socket);
         if (client_socket<0)continue;
+        FD_SET(client_socket,&descriptors);
         if (client_socket>max_fd)max_fd=client_socket;
         printf("client connected\n");
         matchmaking(client_socket);
@@ -67,8 +71,6 @@ int main(int argc, char *argv[] ) {
             send(opp,"opponent left",13,0);
             matchmaking(opp);
           }
-          // reset waiting_fd if it was waiting
-          if (waiting_fd==fd)waiting_fd = 0;
           continue;
         }
         if (strncmp(move,"exit",4)==0) {
@@ -82,14 +84,15 @@ int main(int argc, char *argv[] ) {
         if (strncmp(move,"home",4)==0) {
           // if client wants to leave server, requeue opponent
           close_client(fd,&descriptors);
-          opponent[opp] = -1;
-          matchmaking(opp);
+          if (opp!=-1) {
+            opponent[opp] = -1;
+            matchmaking(opp);
+          }
           continue;
         }
         // send the move if normal behavior
         send(opponent[fd],move,sizeof(move),0);
       }
     }
-
   }
 }
