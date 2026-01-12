2e02bf98bc83f0c30011d4aa23ec4f74c56e1511
Raphael Ramot <raphaelr20@nycstudents.net>
2026-01-12 01:30:06 -0500

    close_client

--- server.c
+++ server.c
@@ -5,9 +5,18 @@ static void sighandler(int signo) {
     exit(0);
   }
 }
-static int client[100];
+
+static int opponent[100];
+static int waiting_fd = -1;
+
 void matchmaking(int client_socket){
 
+}
+
+void close_client(int fd, fd_set descriptors) {
+  FD_CLR(fd,&descriptors);
+  close(fd);
+  if (fd<FD_SETSIZE)opponent[fd] = -1;
 }
 
 int main(int argc, char *argv[] ) {
@@ -30,6 +39,11 @@ int main(int argc, char *argv[] ) {
         if (client_socket>max_fd)max_fd=client_socket;
         printf("client connected\n");
         matchmaking(client_socket);
+      }
+      else {
+        char move[100];
+        int bytes = recv(fd,move,sizeof(move),0);
+        if (bytes<0)err();
       }
     }
     if (FD_ISSET(STDIN_FILENO,&descriptors)) {
